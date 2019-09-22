fb4e226e76ad6c8b7f4949155e69de84ccc6a343
AdamM <ajmrozinski@gmail.com>
2019-09-22 02:20:54 -0400

    required express and path,set env or port

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
