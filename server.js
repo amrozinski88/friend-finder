eb407cb51f0706ab746584ee95c8b3740d0cf724
AdamM <ajmrozinski@gmail.com>
2019-09-22 22:37:45 -0400

    created express path to home and survey page.

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"app/public/home.html"))
+});
+
+app.get("/survey",(req,res)=>{
+    res.sendFile(path.join(__dirname,"app/public/survey.html"))
+});
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
